64c35e19ebdc4bd1abe08b46c59e1d13431974ae
davide-pani <102189224+davide-pani@users.noreply.github.com>
2022-03-25 20:45:56 +0100

    [lib] Added suppress deprecated because handled correctly

--- androidextensions/src/main/java/com/davidepani/androidextensions/text/TextExtensions.kt
+++ androidextensions/src/main/java/com/davidepani/androidextensions/text/TextExtensions.kt
@@ -5,6 +5,7 @@ import android.text.Html
 import android.text.Spanned
 
 
+@Suppress("DEPRECATION")
 fun String.fromHtml(): Spanned {
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
         Html.fromHtml(this, Html.FROM_HTML_MODE_COMPACT)
