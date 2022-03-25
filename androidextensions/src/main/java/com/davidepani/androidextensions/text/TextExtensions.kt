3bebb159a6357836b996e36a9640b09975f1016f
davide-pani <102189224+davide-pani@users.noreply.github.com>
2022-03-25 15:32:41 +0100

    [androidextensions] Added TextExtensions

---
+++ androidextensions/src/main/java/com/davidepani/androidextensions/text/TextExtensions.kt
@@ -0,0 +1,14 @@
+package com.davidepani.androidextensions.text
+
+import android.os.Build
+import android.text.Html
+import android.text.Spanned
+
+
+fun String.fromHtml(): Spanned {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        Html.fromHtml(this, Html.FROM_HTML_MODE_COMPACT)
+    } else {
+        Html.fromHtml(this)
+    }
+}
